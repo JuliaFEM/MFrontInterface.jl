d1d7ac0dea18f11808f3e10dabd17912ce3e9f24
Tero Frondelius <tero.frondelius@gmail.com>
2019-07-10 20:32:27 +0300

    Basic tests to check if the binaries exists

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "MFront.jl" begin
     # Write your own tests here.
+    @test isfile("../deps/usr/lib/mgis-julia.so")
+    @test isfile("../deps/usr/lib/libMFrontGenericInterface.so")
 end
