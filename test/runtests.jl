4eb6af649b0e63e1f11ac4d96369ed5518d69b96
Tero Frondelius <tero.frondelius@gmail.com>
2019-07-11 10:54:19 +0300

    Debugging testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using MFront
 using Test
 
 lpath = abspath(joinpath(dirname(Base.find_package("MFront")),"..","deps","usr","lib"))
+libs = readdir(lpath)
+println(libs)
 
 @testset "MFront.jl" begin
     # Write your own tests here.
