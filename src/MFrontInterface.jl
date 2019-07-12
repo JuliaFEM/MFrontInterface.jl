43ae2289e7cf21619c4a645182d66847f61bde5e
Tero Frondelius <TeroFrondelius@users.noreply.github.com>
2019-07-12 15:58:23 +0300

    Tests with Norton model added (#2)
    * Working Norton test model
    
    * Pinning CxxWrap@0.8.1 for travis
    
    * Cleaning MFrontInterface.jl file

--- src/MFrontInterface.jl
+++ src/MFrontInterface.jl
@@ -1,5 +1,18 @@
 module MFrontInterface
+const lpath = abspath(joinpath(dirname(Base.find_package("MFrontInterface")),"..","deps","usr","lib"))
 
-greet() = print("Hello World!")
+using CxxWrap
+@wrapmodule(joinpath(lpath,"mgis-julia.so"),:define_mgis_module)
+function __init__()
+    @initcxx
+end
 
-end # module
+module behaviour
+const lpath = abspath(joinpath(dirname(Base.find_package("MFrontInterface")),"..","deps","usr","lib"))
+using CxxWrap
+@wrapmodule(joinpath(lpath,"mgis-julia.so"),:define_mgis_behaviour_module)
+function __init__()
+    @initcxx
+end
+end # module behaviour
+end # module MFront`
