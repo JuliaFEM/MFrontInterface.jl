06b49e15c5dda74bd3f45ac2976005ad73f88cf8
Tero Frondelius <tero.frondelius@gmail.com>
2019-07-12 11:09:16 +0300

    First ugly mfront interface

--- src/MFront.jl
+++ src/MFront.jl
@@ -1,5 +1,44 @@
 module MFront
+const lpath = abspath(joinpath(dirname(Base.find_package("MFront")),"..","deps","usr","lib"))
 
-greet() = print("Hello World!")
+function mfront(model)
+    insdir = abspath(joinpath(dirname(Base.find_package("MFront")),"..","deps","usr"))
+    bindir = joinpath(insdir,"bin")
+    curdir = pwd()
+    tmpdir = mktempdir()
 
-end # module
+    cp(model,joinpath(tmpdir,model))
+    cd(tempdir)
+    open("run_mfront.sh","w") do fil
+        write(fil,"export TFELHOME=$insdir\n")
+        write(fil,"export MGISHOME=$insdir\n")
+        write(fil,"export PATH=$bindir\n")
+        write(fil,"export LD_LIBRARY_PATH=$MGISHOME/lib:$LD_LIBRARY_PATH\n")
+        write(fil,"export LD_LIBRARY_PATH=$MGISHOME/lib/julia/mgis:$LD_LIBRARY_PATH\n")
+        write(fil,"export LD_LIBRARY_PATH=$MGISHOME/lib/include:$LD_LIBRARY_PATH\n")
+        write(fil, "mfront --obuild --install-path=$tmpdir --interface=generic $model\n")
+    end
+
+    chmod("run_mfront.sh",0o777)
+    run(`./run_mfront.sh`)
+
+    cp("libBehaviour.so",joinpath(curdir,"libBehaviour.so"))
+    cd(curdir)
+end
+
+
+using CxxWrap
+@wrapmodule(joinpath(lpath,"mgis-julia.so"),:define_mgis_module)
+function __init__()
+    @initcxx
+end
+
+module behaviour
+const lpath = abspath(joinpath(dirname(Base.find_package("MFront")),"..","deps","usr","lib"))
+using CxxWrap
+@wrapmodule(joinpath(lpath,"mgis-julia.so"),:define_mgis_behaviour_module)
+function __init__()
+    @initcxx
+end
+end # module behaviour
+end # module MFront`
