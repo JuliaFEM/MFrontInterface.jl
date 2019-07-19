3441bb98bf0a3d4e96de1ef39c594460b97012f4
Tero Frondelius <tero.frondelius@gmail.com>
2019-07-19 23:25:56 +0300

    show method for BehaviourAllocated

--- src/MFrontInterface.jl
+++ src/MFrontInterface.jl
@@ -28,6 +28,17 @@ export load, BehaviourData, get_variable_offset, get_internal_state_variables
 export get_hypothesis, set_time_increment!, set_external_state_variable!
 export get_final_state, update, get_gradients, get_initial_state, integrate
 export get_initial_state
+
+function Base.show(io::IO,m::BehaviourAllocated)
+    println(io, "libBehaviour: generated from ", get_source(m))
+    println(io, "name of the behaviour: ", get_behaviour(m))
+    print(io, "TFEL version: ", get_tfel_version(m))
+end
+
+function Base.show(io::IO,m::BehaviourDataAllocated)
+    print(io, "Behaviour Data ")
+end
+
 end # module behaviour
 # Re-exporting behaviour model functions
 using .behaviour
