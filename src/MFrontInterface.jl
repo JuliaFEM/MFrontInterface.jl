b2229ad7f3290acf02813bf121752e40f48e806c
Tero Frondelius <TeroFrondelius@users.noreply.github.com>
2019-07-19 09:29:05 +0300

    Making more julian style packet interface (#4)
    * Making more julian style packet interface
    
    * Cleaning out commented stuff

--- src/MFrontInterface.jl
+++ src/MFrontInterface.jl
@@ -14,5 +14,15 @@ using CxxWrap
 function __init__()
     @initcxx
 end
+export load, BehaviourData, get_variable_offset, get_internal_state_variables
+export get_hypothesis, set_time_increment!, set_external_state_variable!
+export get_final_state, update, get_gradients, get_initial_state, integrate
+export get_initial_state
 end # module behaviour
+# Re-exporting behaviour model functions
+using .behaviour
+export load, BehaviourData, get_variable_offset, get_internal_state_variables
+export get_hypothesis, set_time_increment!, set_external_state_variable!
+export get_final_state, update, get_gradients, get_initial_state, integrate
+export get_initial_state
 end # module MFront`
