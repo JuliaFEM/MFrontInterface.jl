fe0664b42238cfcd32b458b383586c6161a56529
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-16 22:00:58 +0000

    build based on ac0e8b6

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MFrontInterface.jl-1","page":"MFrontInterface.jl","title":"MFrontInterface.jl","text":"","category":"section"},{"location":"#","page":"MFrontInterface.jl","title":"MFrontInterface.jl","text":"MFrontInterface.jl wraps MFront.jl to Julia.","category":"page"}]
+}
