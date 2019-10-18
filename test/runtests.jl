66eb37d82ae7cc36e63723b8f114f227227f06f4
Tero Frondelius <tero.frondelius@gmail.com>
2019-10-18 20:15:36 +0300

    Adding uniaxial_increment! testing to ensure Materials.jl compatibility.

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,8 @@ eps = 1.e-12
     include("test_binary_dependencies.jl")
     if Sys.islinux()
         @testset "Norton model" begin include("test_norton_model.jl") end
-        include("test_show_methods.jl")
+
+        @testset "show methods" begin include("test_show_methods.jl") end
 
         @testset "test MFront ideal plastic material model" begin
         include("test_isotropic_linear_hardening_plasticity.jl")
@@ -30,6 +31,10 @@ eps = 1.e-12
 
         @testset "test MFront together with JuAFEM" begin
         include("test_mfront_juafem_3dbeam.jl")
+
+        @testset "test plasticity with uniaxial_increment!" begin
+        include("test_vonmises_uniaxial.jl")
+        end
         end
     end
 end
